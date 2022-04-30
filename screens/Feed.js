82369213f04196fc6e496130fa09aca8678eea64
Ceramicbloon <79235916+Ceramicbloon@users.noreply.github.com>
2022-04-30 12:34:32 -0400

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default class Feed extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Feed</Text>
+            </View>
+        )
+    }
+} 
